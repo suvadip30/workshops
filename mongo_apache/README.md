16c8d30ec03afebf2d0e838f581bfe82e8a419ec
root <root@ip-172-31-24-127.us-west-2.compute.internal>
2020-12-11 04:05:14 +0000

    remove

---
+++ mongo_apache/README.md
@@ -0,0 +1,4 @@
+# mongo_apache
+
+TODO: Enter the cookbook description here.
+
--- mongo_apache/recipes/default.rb
+++
@@ -1,5 +0,0 @@
-#
-# Cookbook:: mongo_apache
-# Recipe:: default
-#
-# Copyright:: 2020, The Authors, All Rights Reserved.
